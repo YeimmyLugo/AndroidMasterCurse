31d4d86f32d591b628f8ba41b46d4b3b400da107
YeimmyLugo <lugokaterin2@gmail.com>
2024-03-08 19:09:18 -0500

    Inicio curso android master 0

---
+++ app/src/main/java/com/example/androidmaster/FirstApp/FirstAppActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidmaster.FirstApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidmaster.R
+
+class FirstAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_app)
+    }
+}
